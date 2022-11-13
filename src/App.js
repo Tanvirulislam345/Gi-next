bb36b07212a75f4284b033f16b37505baec9507a
Tanvirulislam345 <tanvir.topader345@gmail.com>
2022-11-13 18:00:36 +0600

    update fabicons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import LandingPage from './pages/LandingPage';
-import AllBlogsPage from './pages/AllBlogsPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ViewPage from './pages/ViewPage';
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import AllBlogsPage from "./pages/AllBlogsPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ViewPage from "./pages/ViewPage";
+import Favicon from "react-favicon";
+import icon from "../src/assets/icons.png";
 
 function App() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<LandingPage />} />
-                    <Route path='/home' element={<LandingPage />} />
-                    <Route path='/blogs' element={<AllBlogsPage />} />
-                    <Route path='/blogs/:blogId' element={<ViewPage />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <Favicon url={icon} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/blogs" element={<AllBlogsPage />} />
+          <Route path="/blogs/:blogId" element={<ViewPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
